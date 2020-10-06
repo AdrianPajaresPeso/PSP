d981a5573ac4f1cf3d9010aa9012ed3427bbf3b6
AdrianPajaresPeso <dam08.2020.jesuitas@gmail.com>
2020-10-06 06:25:16 +0200

    Ejercicio3 done

---
+++ src/Ejercicio3.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.text.SimpleDateFormat;
+
+public class Ejercicio3 {
+	public void listDirectory(String ruta) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		File directory = new File(ruta);
+		System.out.println("Nombre\t\t\t\tTamaño\t\tUltima Modificacion\t\tRuta\t\t\t\tTipo");
+		System.out.println(
+				"-----------------------------------------------------------------------------------------------------------------------------------------");
+		for (File elementos : directory.listFiles()) {
+
+			if (elementos.isDirectory()) {
+
+				System.out.format("%-25s%8d%5s%30s%30s%20s", elementos.getName(), elementos.length() / 1024, "KB",
+						sdf.format(elementos.lastModified()), elementos.getParent(), "DIRECTORIO");
+				System.out.println();
+			}
+			if (elementos.isFile()) {
+
+				System.out.format("%-25s%8d%5s%30s%30s%20s", elementos.getName(), elementos.length() / 1024, "KB",
+						sdf.format(elementos.lastModified()), elementos.getParent(), "Archivo");
+				System.out.println();
+			}
+
+		}
+
+	}
+}
--- src/Principal.java
+++ src/Principal.java
@@ -4,10 +4,13 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Ejercicio1 e1 = new Ejercicio1();
-		System.out.println(e1.obtenerNumeroRepeticiones("datos.txt", "app"));
+//		System.out.println(e1.obtenerNumeroRepeticiones("datos.txt", "app"));
 		
 		Ejercicio2 e2 = new Ejercicio2();
-		e2.copiarFichero("datos.txt");
+//		e2.copiarFichero("datos.txt");
+		
+		Ejercicio3 e3 = new Ejercicio3();
+		e3.listDirectory("C:/");
 	}
 
 }
