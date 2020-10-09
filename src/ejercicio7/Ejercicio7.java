201a933bccbcea172edc9f2ad76b20726595ef5c
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-10-09 08:49:43 +0200

    Estructura de directorios
    Añadimos una estructura de directorios para poder comprimir mas adelante

--- src/ejercicio7/Ejercicio7.java
+++ src/ejercicio7/Ejercicio7.java
@@ -65,23 +65,25 @@ public class Ejercicio7 {
 	}
 
 	private void option2(Scanner sc) {
-//		sc.nextLine();
-//		System.out.println("Introduce la ruta del directorio a comprimir");
-//		String dir = sc.nextLine();
-//		System.out.println("Introduce el nombre del fichero zip");
-//		String nombre = sc.nextLine();
-//		if (!nombre.contains(".zip")) {
-//			nombre = nombre + ".zip";
-//		}
+		sc.nextLine();
+		System.out.println("Introduce la ruta del directorio a comprimir");
+		String dir = sc.nextLine();
+		System.out.println("Introduce el nombre del fichero zip");
+		String nombre = sc.nextLine();
+		if (!nombre.contains(".zip")) {
+			nombre = nombre + ".zip";
+		}
 
 		try {
-			ZipOutputStream zos = new ZipOutputStream(new FileOutputStream("ficheroZIP.zip"));
+			ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(nombre));
 
 			zos.setLevel(Deflater.DEFAULT_COMPRESSION);
 			zos.setMethod(Deflater.DEFLATED);
 
 			
+			
 			/** Estructura que se repetira en caso de añadir varios ficheros **/
+			
 			ZipEntry entrada = new ZipEntry("fich2.txt");
 			zos.putNextEntry(entrada);
 
