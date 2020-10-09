662e549b452f921777f53a9c8eeff39d12a899ea
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-10-09 10:12:32 +0200

    Comprimir funciona

--- src/ejercicio7/Ejercicio7.java
+++ src/ejercicio7/Ejercicio7.java
@@ -1,16 +1,17 @@
 package ejercicio7;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.zip.Deflater;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
+
+import ejercicio6.Ejercicio6;
 
 public class Ejercicio7 {
 	public void menu7() {
@@ -40,6 +41,7 @@ public class Ejercicio7 {
 	 */
 	private void option1(Scanner sc) {
 		sc.nextLine();// limpiamos el buffer
+		System.out.println("Introduce la ruta del archivo zip que quieres mostrar");
 		try {
 
 			File f = new File(sc.nextLine());
@@ -65,65 +67,79 @@ public class Ejercicio7 {
 	}
 
 	private void option2(Scanner sc) {
-		sc.nextLine();
+		sc.nextLine();// limpiamos el buffer de entrada
+
+		// pedimos la ruta que queremos comprimir
 		System.out.println("Introduce la ruta del directorio a comprimir");
 		String dir = sc.nextLine();
+
+		// pedimos el nombre del fichero zip
 		System.out.println("Introduce el nombre del fichero zip");
 		String nombre = sc.nextLine();
+
+		// si el nomre no contiene la extension zip, lo añadimos
 		if (!nombre.contains(".zip")) {
 			nombre = nombre + ".zip";
 		}
-
+		ZipOutputStream zos = null;
+		FileInputStream fis = null;
 		try {
-			ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(nombre));
+			File fileDir = new File(dir);
+			zos = new ZipOutputStream(new FileOutputStream(nombre));
 
 			zos.setLevel(Deflater.DEFAULT_COMPRESSION);
 			zos.setMethod(Deflater.DEFLATED);
 
-			
-			
-			/** Estructura que se repetira en caso de añadir varios ficheros **/
-			
-			ZipEntry entrada = new ZipEntry("fich2.txt");
-			zos.putNextEntry(entrada);
+			List<File> array = Ejercicio6.listarDirectorios(fileDir);
 
-			FileInputStream fis = new FileInputStream("fich2.txt");
-			byte[] buffer = new byte[1024];
-			int leido = 0;
-			while (0 < (leido = fis.read(buffer))) {
-				zos.write(buffer, 0, leido);
+			for (File archivo : array) {
+
+				/** Estructura que se repetira en caso de añadir varios ficheros **/
+
+				ZipEntry entrada = new ZipEntry(archivo.getPath());
+				zos.putNextEntry(entrada);
+
+				fis = new FileInputStream(archivo.getPath());
+				byte[] buffer = new byte[1024];
+				int leido = 0;
+				while (0 < (leido = fis.read(buffer))) {
+					zos.write(buffer, 0, leido);
+				}
+
 			}
 
 			zos.closeEntry();
-			
 			fis.close();
 			zos.close();
-//			ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(dir+"/"+nombre));
-//			zos.setLevel(Deflater.DEFAULT_COMPRESSION);
-//			zos.setMethod(Deflater.DEFLATED);
-//
-//			ZipEntry entrada = new ZipEntry(nombre);
-//			zos.putNextEntry(entrada);
-//			File f = new File(dir);
-//			List<File> listaFicheros = Ejercicio6.listarDirectorios(f);
-//			FileInputStream fis = null;
-//			for (File archivo : listaFicheros) {
-//				fis = new FileInputStream(archivo.getAbsolutePath());
-//				byte buffer[]= new byte[1024];
-//				int leido = 0;
-//				while(0<(leido = fis.read(buffer))) {
-//					zos.write(buffer, 0, leido);
-//				}
-//			}
-//			
-//			zos.closeEntry();
-//			fis.close();
-//			zos.close();
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+
+		} finally {
+			try {
+
+				zos.closeEntry();
+				fis.close();
+				zos.close();
+
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
 		}
+	}
+
+	private Boolean existeZip(File dir, String nombre) {
+		Boolean flag = false;
+		List<File> listaArchivos = Ejercicio6.listarDirectorios(dir);
+
+		for (File file : listaArchivos) {
+			if (file.getName().equals(nombre)) {
+				flag = true;
+			}
+		}
+
+		return flag;
 	}
 
 	private void printMenu() {
@@ -133,7 +149,9 @@ public class Ejercicio7 {
 		System.out.println("\t3. Descomprimir un fichero .zip");
 		System.out.println("\t4. Salir.");
 
-		System.out.println("~$:");
+		
 	}
+	
+	
 
 }
