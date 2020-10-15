8887ea32ca448e8b52490cd9131404e370aaf2e4
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-10-15 11:09:31 +0200

    final commit

--- src/ejercicio2/Ejercicio2.java
+++ src/ejercicio2/Ejercicio2.java
@@ -2,7 +2,6 @@ package ejercicio2;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
