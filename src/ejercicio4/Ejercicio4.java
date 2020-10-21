18641341739cacbccc4baad8a2b7f87d6a732644
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-10-21 11:31:03 +0200

    develop / ejercicio4 empezado

--- src/ejercicio4/Ejercicio4.java
+++ src/ejercicio4/Ejercicio4.java
@@ -1,6 +1,25 @@
 package ejercicio4;
 
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
 public class Ejercicio4 {
 	
-	
+	public void generateQR() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Mensaje que quieras usar para crear el codigo QR");
+		String msg = sc.nextLine();
+		try {
+			URL link = new URL("https://api.qrserver.com/v1/create-qr-code/?data="+msg);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
 }
