8ffe4cfd01d61bebadabe4699b6ab8eb13894026
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-10-21 11:17:27 +0200

    develop / ejercicio3

--- src/ejercicio4/Ejercicio4.java
+++ src/ejercicio4/Ejercicio4.java
@@ -1,9 +1,4 @@
 package ejercicio4;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
 
 public class Ejercicio4 {
 	
