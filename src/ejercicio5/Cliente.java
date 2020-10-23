e9bde43a29ee3016fd405cd0110f4cf478ddab03
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-10-23 09:25:16 +0200

    sadasd

--- src/ejercicio5/Cliente.java
+++ src/ejercicio5/Cliente.java
@@ -1,12 +1,10 @@
 package ejercicio5;
 
-public class Principal {
+public class Cliente {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Ejercicio5 e = new Ejercicio5();
-		e.readCSV();
-		
+
 	}
 
 }
