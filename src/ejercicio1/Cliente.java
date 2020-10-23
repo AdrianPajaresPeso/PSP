362d65aed2b1911ca4bf07c79d572c9f6582ad81
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-10-23 10:00:46 +0200

    develop / ejercicio 2 Servers

--- src/ejercicio1/Cliente.java
+++ src/ejercicio1/Cliente.java
@@ -1,10 +1,31 @@
 package ejercicio1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+
 public class Cliente {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			Scanner sc = new Scanner (System.in);
+			Socket cliente = new Socket("localhost", 6000);
+			PrintStream ps = new PrintStream(cliente.getOutputStream());
+			System.out.println("Escribe el mensaje que quieras enviar:");
+			ps.println(sc.nextLine());
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+			String msg = br.readLine();
+			System.out.println(msg);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/ejercicio2/Server.java
+++ src/ejercicio2/Server.java
@@ -1,9 +1,38 @@
 package ejercicio2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			ServerSocket server = new ServerSocket(6000);
+			while (true) {
+				System.out.println("Esperando Conexion...");
+				Socket cliente = server.accept();
+				
+				System.out.println(cliente.getInetAddress().getHostAddress()+" se ha conectado");
+				BufferedReader br = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+				
+				String msg = br.readLine();
+				System.out.println("Mensaje recibido: <"+msg+">");
+				PrintStream ps = new PrintStream(cliente.getOutputStream());
+				System.out.println("Devolviendo mensaje");
+				ps.println(msg);
+				
+				ps.close();
+				br.close();
+				cliente.close();
+			}
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
 
 	}
 
