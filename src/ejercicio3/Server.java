67892f13d456053176f319add4bedac86dc47398
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-10-26 08:40:08 +0100

    develop / ejercicio 3

--- src/ejercicio3/Server.java
+++ src/ejercicio3/Server.java
@@ -1,9 +1,31 @@
 package ejercicio3;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			ServerSocket server = new ServerSocket(6000);
+
+			while (true) {
+				System.out.println("Esperando conexión...");
+				Socket cliente = server.accept();
+
+				System.out.println(cliente.getInetAddress().getHostAddress() + " se ha conectado");
+				
+				PrintStream ps = new PrintStream(cliente.getOutputStream());
+				Date today  = new Date();
+				String date = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+				ps.println(date);
+			}
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
 
 	}
 
